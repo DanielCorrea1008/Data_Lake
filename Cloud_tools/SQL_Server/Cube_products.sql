0635f2d45f44c132d2de7ac2b04cd5181c6b62fd
DanielCorrea1008 <90923616+DanielCorrea1008@users.noreply.github.com>
2022-11-08 20:46:51 -0500

    Update and rename Cube_products to Cube_products.sql

---
+++ Cloud_tools/SQL_Server/Cube_products.sql
@@ -0,0 +1,11 @@
+ create table empleados(
+  nombre varchar(30),
+  documento char(8),
+  domicilio varchar(30),
+  seccion varchar(20),
+  sueldo decimal(6,2),
+  cantidadhijos tinyint,
+  fechaingreso datetime,
+  primary key(documento)
+ );
+
